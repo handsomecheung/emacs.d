17d7c3856980027b7e5aaf042e345da7b75a553a
Handsome Cheung <handsomecheung@gmail.com>
2013-05-06 23:56:14 +0800

    multi-windows controller

--- init-windows.el
+++ init-windows.el
@@ -37,4 +37,34 @@
 (global-set-key "\C-x_" 'split-window-vertically-instead)
 
 
+
+(require 'window-number)
+(autoload 'window-number-mode "window-number"
+  "A global minor mode that enables selection of windows according to
+numbers with the C-x C-j prefix.  Another mode,
+`window-number-meta-mode' enables the use of the M- prefix."
+  t)
+
+(autoload 'window-number-meta-mode "window-number"
+  "A global minor mode that enables use of the M- prefix to select
+windows, use `window-number-mode' to display the window numbers in
+the mode-line."
+  t)
+
+(window-number-mode 1)
+(window-number-meta-mode 1)
+
+
+(winner-mode 1)
+;; copied from http://puntoblogspot.blogspot.com/2011/05/undo-layouts-in-emacs.html
+(global-set-key (kbd "C-x 4 u") 'winner-undo)
+(global-set-key (kbd "C-x 4 r") 'winner-redo)
+
+(require 'buffer-move)
+(global-set-key (kbd "C-c C-v C-k")     'buf-move-up)
+(global-set-key (kbd "C-c C-v C-j")   'buf-move-down)
+(global-set-key (kbd "C-c C-v C-h")   'buf-move-left)
+(global-set-key (kbd "C-c C-v C-l")  'buf-move-right)
+
+
 (provide 'init-windows)
