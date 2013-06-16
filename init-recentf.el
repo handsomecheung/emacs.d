05bf8d625efd5156d9a8f5e15a477e2608e317e7
Handsome Cheung <handsomecheung@gmail.com>
2013-06-16 22:07:50 +0800

    bug fixed

--- init-recentf.el
+++ init-recentf.el
@@ -1,6 +1,6 @@
 (recentf-mode 1)
 (setq recentf-max-saved-items 1000
       recentf-exclude '("/tmp/" "/ssh:"))
-(global-set-key "C-x C-h" 'recentf-open-files)
+(global-set-key (kbd "C-x C-h") 'recentf-open-files)
 
 (provide 'init-recentf)
