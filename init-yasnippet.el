80b2358fc1faf397a8efa885bbadc2e9bdfd779e
Handsome Cheung <handsomecheung@gmail.com>
2013-09-11 10:34:56 +0800

    yas root dir

--- init-yasnippet.el
+++ init-yasnippet.el
@@ -1,6 +1,6 @@
 ;(require-package 'yasnippet)
 (require 'yasnippet)
-(setq yas-root-directory "~/.emacs.d/elpa/yasnippet-20130722.1832/snippets")
+(setq yas-root-directory "~/.emacs.d/elpa/yasnippet-20130902.1201/snippets")
 
 (yas-load-directory yas-root-directory)
 (setq yas-snippet-dirs yas-root-directory)
