b554990cd3a52680ac0796b9c2d3277d27a4e9c5
Handsome Cheung <handsomecheung@gmail.com>
2013-09-20 23:30:53 +0800

    get-line-string and nth function

--- init-common-function.el
+++ init-common-function.el
@@ -5,8 +5,22 @@ White space here is any of: space, tab, emacs newline (line feed, ASCII 10)."
 
 (defun insert-list-newline (lst)
   "insert list, each of a newline"
-  (if (not (equal lst nil)) (progn (insert (trim-string (car lst)))
-                                   (newline)
-                                   (insert-list-newline (cdr lst)))))
+  (if (not (equal lst nil))
+      (let* ((first-item (car lst))
+             (string (if (equal first-item nil) "" first-item)))
+        (progn (insert (trim-string string))
+               (newline)
+               (insert-list-newline (cdr lst))))))
+
+(defun get-line-string (line-number)
+  "get specific line string"
+  (save-excursion (progn (goto-line line-number)
+          (trim-string (buffer-substring-no-properties (line-beginning-position) (line-end-position))))))
+
+(defun nth (n list)
+  "Returns the Nth element of LIST.
+     N counts from zero.  If LIST is not that long, nil is returned."
+  (car (nthcdr n list)))
+
 
 (provide 'init-common-function)
