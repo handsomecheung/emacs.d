5ac73a247c8bc6ac6f40ca857a40056bab2af67b
Handsome Cheung <handsomecheung@gmail.com>
2013-09-24 09:16:52 +0800

    snippet for work

--- init-yasnippet.el
+++ init-yasnippet.el
@@ -1,9 +1,13 @@
 ;(require-package 'yasnippet)
 (require 'yasnippet)
 (setq yas-root-directory "~/.emacs.d/elpa/yasnippet-20130907.1855/snippets")
+(setq my-yasnippet-directory "~/.emacs.d/my-yasnippet")
 
 (yas-load-directory yas-root-directory)
-(setq yas-snippet-dirs yas-root-directory)
+(setq yas-snippet-dirs (list yas-root-directory my-yasnippet-directory))
+
+(setq yas/prompt-functions '(yas/x-prompt yas/dropdown-prompt))
+
 (yas-global-mode)
 (yas-minor-mode-on)
 
