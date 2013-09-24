1f3082447928f5e80f97bf52a9c15049f3646194
Handsome Cheung <handsomecheung@gmail.com>
2013-09-24 09:42:22 +0800

    multiple cursor for evil

--- init-editing-utils.el
+++ init-editing-utils.el
@@ -112,7 +112,9 @@
 (global-set-key (kbd "C-;") 'ace-jump-mode)
 (global-set-key (kbd "C-:") 'ace-jump-word-mode)
 
-
+;;--------------------------------------------------------------------------------
+;;   multiple-cursors
+;;--------------------------------------------------------------------------------
 (require-package 'multiple-cursors)
 ;; multiple-cursors
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
@@ -133,30 +135,28 @@
 
 ;;; Thanks to tkf on
 ;;; https://github.com/magnars/multiple-cursors.el/issues/19
-;;; insert state has been changed to emacs state
-;; (defvar my-mc-evil-previous-state nil)
+; insert state has been changed to emacs state
+(defvar my-mc-evil-previous-state nil)
 
-;; (defun my-mc-evil-switch-to-emacs-state ()
-;;   (when (and (bound-and-true-p evil-mode)
-;;              (not (eq evil-state 'emacs)))
-;;     (setq my-mc-evil-previous-state evil-state)
-;;     (evil-emacs-state)))
+(defun my-mc-evil-switch-to-emacs-state ()
+  (when (and (bound-and-true-p evil-mode)
+             (not (eq evil-state 'emacs)))
+    (setq my-mc-evil-previous-state evil-state)
+    (evil-emacs-state)))
 
-;; (defun my-mc-evil-back-to-previous-state ()
-;;   (when my-mc-evil-previous-state
-;;     (unwind-protect
-;;         (case my-mc-evil-previous-state
-;;           ((normal visual insert) (evil-force-normal-state))
-;;           (t (message "Don't know how to handle previous state: %S"
-;;                       my-mc-evil-previous-state)))
-;;       (setq my-mc-evil-previous-state nil))))
+(defun my-mc-evil-back-to-previous-state ()
+  (when my-mc-evil-previous-state
+    (unwind-protect
+        (case my-mc-evil-previous-state
+          ((normal visual insert) (evil-force-normal-state))
+          (t (message "Don't know how to handle previous state: %S"
+                      my-mc-evil-previous-state)))
+      (setq my-mc-evil-previous-state nil))))
 
-;; (add-hook 'multiple-cursors-mode-enabled-hook
-;;           'my-mc-evil-switch-to-emacs-state)
-;; (add-hook 'multiple-cursors-mode-disabled-hook
-;;           'my-mc-evil-back-to-previous-state)
-
-
+(add-hook 'multiple-cursors-mode-enabled-hook
+          'my-mc-evil-switch-to-emacs-state)
+(add-hook 'multiple-cursors-mode-disabled-hook
+          'my-mc-evil-back-to-previous-state)
 ;;; --------------------------------------------------------------------------------
 
 (defun duplicate-line ()
