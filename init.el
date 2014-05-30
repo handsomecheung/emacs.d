a2d81e12280c61aa39f17c6a9d31235b0a038a94
Handsome Cheung <handsomecheung@gmail.com>
2014-05-30 08:57:33 +0800

    disable session

--- init.el
+++ init.el
@@ -76,7 +76,7 @@
 (require 'init-auto-complete)
 (require 'init-yasnippet)
 (require 'init-windows)
-(require 'init-sessions)
+;; (require 'init-sessions)
 (require 'init-fonts)
 (require 'init-mmm)
 (require 'init-growl)
