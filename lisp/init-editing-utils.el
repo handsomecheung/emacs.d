1c82a296da6409c13c35f1652d39940ce3ed47f3
Shuai Z <handsomecheung@gmail.com>
2019-01-29 11:28:07 +0800

    comment key bindings for multiple cursors in region

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -124,13 +124,13 @@
 ;; multiple-cursors
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
-;; (global-set-key (kbd "C-+") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
 ;; From active region to multiple cursors:
-(global-set-key (kbd "C-c c r") 'set-rectangular-region-anchor)
-(global-set-key (kbd "C-c c c") 'mc/edit-lines)
-(global-set-key (kbd "C-c c e") 'mc/edit-ends-of-lines)
-(global-set-key (kbd "C-c c a") 'mc/edit-beginnings-of-lines)
+;; (global-set-key (kbd "C-c c r") 'set-rectangular-region-anchor)
+;; (global-set-key (kbd "C-c c c") 'mc/edit-lines)
+;; (global-set-key (kbd "C-c c e") 'mc/edit-ends-of-lines)
+;; (global-set-key (kbd "C-c c a") 'mc/edit-beginnings-of-lines)
 
 ;;; autopair messes with multiple cursors, so disable it
 (add-hook 'multiple-cursors-mode-enabled-hook (lambda ()
