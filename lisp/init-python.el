5098ae9e2f78429625004a78ee37372700a6f250
hc <handsomecheung@gmail.com>
2019-09-07 15:04:04 +0800

    set flake8 config path

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -22,6 +22,16 @@
  '(flycheck-python-flake8-executable "python3")
  '(flycheck-python-pycompile-executable "python3")
  '(flycheck-python-pylint-executable "python3"))
+
+;; (add-hook 'python-mode-hook 'flycheck-mode)
+(setq flycheck-flake8rc "~/.flake8")
+
+;; You can set the path to your flake8 configuration with:  (setq flycheck-flake8rc "/path/to/your/flake8-config-file")
+
+;; As for enabling there are different ways of doing it I prefer to use an add-hook like so: (add-hook 'python-mode-hook 'flycheck-mode)
+
+;; You can also set it up globally with: (add-hook 'after-init-hook #'global-flycheck-mode).
+
 ;;----------------------------------------------------------------------------
 
 (provide 'init-python)
