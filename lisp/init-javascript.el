69a891490ac3a8e24ce8737ef1fb971f4fcbe91c
hc <handsomecheung@gmail.com>
2020-05-30 13:22:58 +0800

    lint indent 2

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -91,8 +91,16 @@
 ;; major mode for editing web templates. which can handle mixed js and html like jsx
 ;; -----------------------------------
 (require-package 'web-mode)
+
 (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
 (flycheck-add-mode 'javascript-eslint 'web-mode)
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+)
+(add-hook 'web-mode-hook  'my-web-mode-hook)
 ;; -----------------------------------
 
 
